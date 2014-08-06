8b9ca9542a413e8f7f890d87e6e2abc305568822
git <stefan.thies@me.com>
2014-08-06 19:01:24 +0200

    support of https and multiple nodes (round robin)

--- README.md
+++ README.md
@@ -42,6 +42,9 @@ npm test
 ```js
 var crate = require('node-crate');
 crate.connect ('localhost', 4200)
+// or crate.connect ('http://localhost:4200')
+// to use multiple nodes in round robin crate.connect ('http://host1:4200 http://host2:4200')
+// to use https crate.connect ('https://host1:4200 https://host2:4200')
 crate.execute ("select * from tweets where text like ? and retweed=? limit 1", ['Frohe Ostern%', true]).success (function (res){
 	// res.json is an array with JSON object, with column names as properties, TIMESTAMP is converted to Date for crate V0.38+
 	// res.cols are column names
---
+++ connection-pool.js
@@ -0,0 +1,94 @@
+// some workarounds to make it work without major changes on node-crate
+// TODO: handling of connection fails
+// currently round robin to the nodes - other startegies, like main node, fallback nodes
+// needs to be implemented
+
+var http = require ('http');
+var https = require ('https')
+var urlParser = require ('url')
+
+var optionTemplate = {
+    host: 'localhost',
+    path: '/_sql?types',
+    port: '4200',
+    method: 'POST',
+    headers: {
+        'Connection': 'keep-alive'
+    }
+};
+var _options = {https: false, nodes: ['localhost:4200'] };
+var httpOptions = []
+var httpOptionsBlob = []
+var lastUsed = 0;
+
+function getNextNodeOptions(type)
+{
+	lastUsed++
+	if (lastUsed > httpOptions.length-1)
+		lastUsed = 0;
+	if (type == 'blob') 
+		return httpOptionsBlob[lastUsed];
+	else 
+		return httpOptions [lastUsed];
+
+}
+
+function getRequest (nodeOptions, callback)
+{
+	if (nodeOptions.protocol == 'https')
+		return https.request (nodeOptions.httpOpt, callback) 
+	else
+		return http.request (nodeOptions.httpOpt, callback)
+}
+
+
+function parseStringOptions (opt)
+{
+	var nodes = opt.split (' ');
+	var nodeInfos = nodes.map (function (node) {
+		var urlFields = urlParser.parse (node)
+		urlFields.protocol = urlFields.protocol.replace (':','')
+		return urlFields;
+	})
+	return nodeInfos;
+}
+// optionString e.g. "https://localhost:9200 http://myserver:4200"
+exports.connect = function (optionString)
+{
+	var options = parseStringOptions (optionString)
+	
+	options.forEach (function (e){
+		//console.log (e.hostname)
+		//console.log (e.port)
+		httpOptions.push ({
+			httpOpt: {
+				host: e.hostname,
+				port: e.port || 4200,
+				path: '/_sql?types',
+				method: 'POST',
+		    	headers: {
+		     	   'Connection': 'keep-alive'
+		    	}
+		    },
+			protocol: e.protocol
+		})
+		httpOptionsBlob.push (e.protocol +'://' + e.hostname + ':' + e.port + '/_blobs/')
+	})
+}
+
+
+
+exports.getSqlRequest = function (callback) {
+	var options = getNextNodeOptions('sql')
+	//console.log (options)
+	return getRequest (options, callback);
+}
+
+exports.getBlobUrl = function (callback) {
+	return options = getNextNodeOptions('blob')
+	//return options.protocol + '://' +  options.httpOpt.hostname + ':'  + options.httpOpt.port
+}
+
+exports.getHttpOptions = function () {
+	return getNextNodeOptions('sql').httpOpt;
+}
