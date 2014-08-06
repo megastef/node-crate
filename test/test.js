d969a581215ad92ded506adb3f4ec2f1c04296a8
Martin Heidegger <martin.heidegger@gmail.com>
2014-08-07 04:09:28 +0900

    Cleaned the code - jslinted everything - used lab instead of mocha (because smaller) - moved code to lib - fixed a tiny

--- test/test.js
+++ test/test.js
@@ -1,158 +1,140 @@
-var crate = require('../node-crate'),
-	should = require('should');
+"use strict";
 
-crate.connect('localhost', 4200)
+var Lab = require("lab"),
+    crate = require('../'),
+    should = require('should'),
+    describe = Lab.experiment,
+    it = Lab.test,
+    expect = Lab.expect;
 
-describe('#node-crate', function() {
-	it('Create table', function(done) {
-		schema = {NodeCrateTest: {id: 'integer primary key', title: 'string'}}
-		crate.create (schema)
-			.success(function(res) {
-				res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
+crate.connect('localhost', 4200);
 
-	})
+describe('#node-crate', function () {
+    it('Create table', function (done) {
+        var schema = {NodeCrateTest: {id: 'integer primary key', title: 'string'}};
+        crate.create(schema)
+            .success(function (res) {
+                expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	it('Create blob table', function(done) {
-		
-		crate.createBlobTable ('blobtest',3,1)
-			.success(function(res) {
-				//res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
+    it('Create blob table', function (done) {
+        crate.createBlobTable('blobtest', 3, 1)
+            .success(function () {
+                //expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	})
-	var hashkey = '';
-	it('Insert Blob', function(done) {
-		var buffer = new Buffer ([1])
-		crate.insertBlob('blobtest', buffer)
-			.success(function(res) {
-				//res.rowcount.should.be.exactly(1);
-				hashkey = res;
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
+    var hashkey = '';
 
-	})
+    it('Insert Blob', function (done) {
+        var buffer = new Buffer([1]);
+        crate.insertBlob('blobtest', buffer)
+            .success(function (res) {
+                //expect(res.rowcount).to.be.equal(1);
+                hashkey = res;
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	
+    it('Insert', function (done) {
+        crate.insert('NodeCrateTest', {
+            id: '1',
+            title: 'Title'
+        })
+            .success(function (res) {
+                expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	it('Insert', function(done) {
+    it('Select', function (done) {
 
-		crate.insert('NodeCrateTest', {
-			id: '1',
-			title: 'Title'
-		})
-			.success(function(res) {
-				res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
+        setTimeout(function () {
+            crate.execute('SELECT * FROM NodeCrateTest limit 100')
+                .success(function (res) {
+                    expect(res.rowcount).to.be.equal(1);
+                    done();
+                })
+                .error(function (err) {
+                    done(err);
+                });
+        }, 5000);
+    });
 
-	})
+    it('Update', function (done) {
+        crate.update('NodeCrateTest', {
+            title: 'TitleNew'
+        }, 'id=1')
+            .success(function (res) {
+                expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	it('Select', function(done) {
+    it('Select', function (done) {
+        setTimeout(function () {
+            crate.execute('SELECT * FROM NodeCrateTest limit 100')
+                .success(function (res) {
+                    expect(res.json[0].title).to.be.equal('TitleNew');
+                    done();
+                })
+                .error(function (err) {
+                    done(err);
+                });
+        }, 5000);
+    });
 
-		this.timeout(6000);
+    it('getBlob', function (done) {
+        crate.getBlob('blobtest', hashkey)
+            .success(function () {
+                //expect(data.toString()).to.be.equal('1');
+                //hashkey = res;
+                // WE GET THIS "[.blob_blobtest] missing\n", have to check why, maybe refresh is to high ...
+                // until this is clear, lets pass the test when we get success
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-		setTimeout(function() {
-			crate.execute('SELECT * FROM NodeCrateTest limit 100')
-				.success(function(res) {
-					res.rowcount.should.be.exactly(1);
-					done();
-				})
-				.error(function(err) {
-					done(err);
-				})
-		}, 5000);
+    it('Delete', function (done) {
+        crate.delete('NodeCrateTest', 'id=1')
+            .success(function (res) {
+                expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
 
-	})
-
-	it('Update', function(done) {
-
-		crate.update('NodeCrateTest', {
-			title: 'TitleNew'
-		}, 'id=1')
-			.success(function(res) {
-
-				res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				console.log('err');
-				done(err);
-			})
-
-	})
-
-	it('Select', function(done) {
-
-		this.timeout(6000);
-
-		setTimeout(function() {
-			crate.execute('SELECT * FROM NodeCrateTest limit 100')
-				.success(function(res) {
-					res.json[0].title.should.be.exactly('TitleNew');
-					done();
-				})
-				.error(function(err) {
-					done(err);
-				})
-		}, 5000);
-
-	})
-
-	it('getBlob', function(done) {
-		
-		crate.getBlob('blobtest', hashkey)
-			.success(function(data) {
-				console.log (data.toString('utf-8'))
-				//data.toString().should.be.exactly('1');
-				//hashkey = res;
-				// WE GET THIS "[.blob_blobtest] missing\n", have to check why, maybe refresh is to high ...
-				// until this is clear, lets pass the test when we get success
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
-
-	})
-
-	it('Delete', function(done) {
-
-		crate.delete('NodeCrateTest', 'id=1')
-			.success(function(res) {
-				res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				console.log('err');
-				done(err);
-			})
-
-	})
-
-	it('Delete table', function(done) {
-
-		crate.drop('NodeCrateTest')
-			.success(function(res) {
-				res.rowcount.should.be.exactly(1);
-				done();
-			})
-			.error(function(err) {
-				done(err);
-			})
-	})
-})
+    it('Delete table', function (done) {
+        crate.drop('NodeCrateTest')
+            .success(function (res) {
+                expect(res.rowcount).to.be.equal(1);
+                done();
+            })
+            .error(function (err) {
+                done(err);
+            });
+    });
+});
