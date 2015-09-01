85ef161e32e6c01f994698729195b17aa336e008
megastef <s.thies@bigdata-analyst.de>
2015-09-01 16:04:31 +0200

    moved to 'standard' code format and mocha for tests

--- test/lint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/*jslint nomen: true*/
-var dirname = __dirname;
-/*jslint nomen: false */
-
-var Lab = require("lab");
-
-require("lab-lint")(Lab, dirname);
--- test/test.js
+++ test/test.js
@@ -1,162 +1,167 @@
-"use strict";
+'use strict'
+/* global describe, it */
+var expect = require('expect')
+// var Lab = require("lab"),
+var crate = require('../')
+// describe = Lab.experiment,
+// it = Lab.test,
+// expect = Lab.expect
 
-var Lab = require("lab"),
-    crate = require('../'),
-    describe = Lab.experiment,
-    it = Lab.test,
-    expect = Lab.expect;
-
-crate.connect('http://127.0.0.1:4200');
+crate.connect('http://127.0.0.1:4200')
 
 describe('#node-crate', function () {
-    it('Create blob table', function (done) {
-        crate.createBlobTable('blob_test', 0, 1)
-            .success(function () {
-                // expect(res.rowcount).to.be.equal(1);
-                // console.log(res);
-                done();
-            })
-            .error(function (err) {
-                console.log(err);
-                done(err);
-            });
-    });
+  it('Create blob table', function (done) {
+    this.timeout = 30000
+    crate.createBlobTable('blob_test', 0, 1)
+      .success(function () {
+        // expect(res.rowcount).toBe(1)
+        // console.log(res)
+        done()
+      })
+      .error(function (err) {
+        console.log(err)
+        done(err)
+      })
+  })
 
-    it('Create table', function (done) {
-        var schema = {NodeCrateTest: {id: 'integer primary key', title: 'string'}};
-        crate.create(schema)
-            .success(function (res) {
-                expect(res.rowcount).to.be.equal(1);
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+  it('Create table', function (done) {
+    var schema = {NodeCrateTest: {id: 'integer primary key', title: 'string'}}
+    crate.create(schema)
+      .success(function (res) {
+        expect(res.rowcount).toBe(1)
+        done()
+      })
+      .error(function (err) {
+        done(err)
+      })
+  })
 
-    var hashkey = '';
+  var hashkey = ''
 
-    it('Insert Blob', function (done) {
-        setTimeout(function () {
-            // var buffer = new Buffer([1,3,4]);
-            crate.insertBlobFile('blob_test', './lib/index.js')
-                .success(function (res) {
-                    //console.log(res);
-                    //expect(res.rowcount).to.be.equal(1);
-                    hashkey = res;
-                    done();
-                })
-                .error(function (err) {
-                    console.log(err);
-                    // crate returned an error, but it does not mean that the driver behaves wrong.
-                    // In this case we get HTTP 500 only on drone.io, we need to check why
-                    done();
-                });
-        }, 1000);
-    });
-
-    it('Insert', function (done) {
-        crate.insert('NodeCrateTest', {
-            id: '1',
-            title: 'Title',
-            numberVal: 42
+  it('Insert Blob', function (done) {
+    setTimeout(function () {
+      // var buffer = new Buffer([1,3,4])
+      crate.insertBlobFile('blob_test', './lib/index.js')
+        .success(function (res) {
+          // console.log(res)
+          // expect(res.rowcount).toBe(1)
+          hashkey = res
+          done()
         })
-            .success(function (res) {
-                expect(res.rowcount).to.be.equal(1);
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+        .error(function (err) {
+          console.log(err)
+          // crate returned an error, but it does not mean that the driver behaves wrong.
+          // In this case we get HTTP 500 only on drone.io, we need to check why
+          done()
+        })
+    }, 100)
+  })
 
-    it('Select', function (done) {
+  it('Insert', function (done) {
+    setTimeout(function () {
+      crate.insert('NodeCrateTest', {
+        id: '1',
+        title: 'Title',
+        numberVal: 42
+      })
+        .success(function (res) {
+          expect(res.rowcount).toBe(1)
+          done()
+        })
+        .error(function (err) {
+          done(err)
+        })
+    }, 500)
+  })
 
-        setTimeout(function () {
-            crate.execute('SELECT * FROM NodeCrateTest limit 1')
-                .success(function (res) {
-                    expect(res.rowcount).to.be.equal(1);
-                    done();
-                })
-                .error(function (err) {
-                    done(err);
-                });
-        }, 1000);
-    });
+  it('Select', function (done) {
+    setTimeout(function () {
+      crate.execute('SELECT * FROM NodeCrateTest limit 1')
+        .success(function (res) {
+          expect(res.rowcount).toBe(1)
+          done()
+        })
+        .error(function (err) {
+          done(err)
+        })
+    }, 1000)
+  })
 
-    it('Update', function (done) {
-        crate.update('NodeCrateTest', {
-            title: 'TitleNew'
-        }, 'id=1')
-            .success(function (res) {
-                expect(res.rowcount).to.be.equal(1);
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+  it('Update', function (done) {
+    setTimeout(function () {
+      crate.update('NodeCrateTest', {
+        title: 'TitleNew'
+      }, 'id=1')
+        .success(function (res) {
+          expect(res.rowcount).toBe(1)
+          done()
+        })
+        .error(function (err) {
+          done(err)
+        })
+    }, 2000)
+  })
 
-    it('Select', function (done) {
-        setTimeout(function () {
-            crate.execute('SELECT * FROM NodeCrateTest limit 100')
-                .success(function (res) {
-                    expect(res.json[0].title).to.be.equal('TitleNew');
-                    expect(res.json[0].numberVal).to.be.equal(42);
-                    done();
-                })
-                .error(function (err) {
-                    done(err);
-                });
-        }, 1000);
-    });
+  it('Select after update', function (done) {
+    setTimeout(function () {
+      crate.execute('SELECT * FROM NodeCrateTest limit 100')
+        .success(function (res) {
+          expect(res.json[0].title).toBe('TitleNew')
+          expect(res.json[0].numberVal).toBe(42)
+          done()
+        })
+        .error(function (err) {
+          done(err)
+        })
+    }, 3000)
+  })
 
-    it('getBlob', function (done) {
-        crate.getBlob('blobtest', hashkey)
-            .success(function () {
-                //expect(data.toString()).to.be.equal('1');
-                //hashkey = res;
-                // WE GET THIS "[.blob_blobtest] missing\n", have to check why, maybe refresh is to high ...
-                // until this is clear, lets pass the test when we get success
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+  it('getBlob', function (done) {
+    crate.getBlob('blobtest', hashkey)
+      .success(function () {
+        // expect(data.toString()).toBe('1')
+        // hashkey = res
+        // WE GET THIS "[.blob_blobtest] missing\n", have to check why, maybe refresh is to high ...
+        // until this is clear, lets pass the test when we get success
+        done()
+      })
+      .error(function (err) {
+        done(err)
+      })
+  })
 
-    it('Delete', function (done) {
-        crate.delete('NodeCrateTest', 'id=1')
-            .success(function (res) {
-                expect(res.rowcount).to.be.equal(1);
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+  it('Delete', function (done) {
+    crate.delete('NodeCrateTest', 'id=1')
+      .success(function (res) {
+        expect(res.rowcount).toBe(1)
+        done()
+      })
+      .error(function (err) {
+        done(err)
+      })
+  })
 
-    it('Drop table', function (done) {
-        crate.drop('NodeCrateTest')
-            .success(function (res) {
-                expect(res.rowcount).to.be.equal(1);
-                done();
-            })
-            .error(function (err) {
-                done(err);
-            });
-    });
+  it('Drop table', function (done) {
+    crate.drop('NodeCrateTest')
+      .success(function (res) {
+        expect(res.rowcount).toBe(1)
+        done()
+      })
+      .error(function (err) {
+        done(err)
+      })
+  })
 
-    it('Drop Blob Table', function (done) {
-        setTimeout(function () {
-            crate.dropBlobTable('blob_test')
-                .success(function () {
-                    // expect(res.rowcount).to.be.equal(1);
-                    done();
-                })
-                .error(function (err) {
-                    done(err);
-                });
-        }, 6000);
-    });
-});
+  it('Drop Blob Table', function (done) {
+    setTimeout(function () {
+      crate.dropBlobTable('blob_test')
+        .success(function () {
+          // expect(res.rowcount).toBe(1)
+          done()
+        })
+        .error(function (err) {
+          done(err)
+        })
+    }, 6000)
+  })
+})
